1f46f9d6a92f5dbb9ecf066a478b55dd0ff8b840
aKwong-Taylor <a.kwong-taylor@outlook.com>
2019-12-02 15:56:31 -0800

    Finished all API calls.

--- server/routes/api/settings.js
+++ server/routes/api/settings.js
@@ -15,26 +15,164 @@ router.get("/:user", async (req, res) => {
     }
 }); 
 
-/// PATCH a certain setting for a certain user ---> DOUBLE CHECK THIS ONLY WORKS WITH :SETTING HARD-CODED 
-router.patch("/:user/:setting", async (req, res) => { 
+/// PATCH Birb ---> DONE
+router.patch("/:user/birb", async (req, res) => { 
     const preferences = await User.findById(req.params.user, "preferences"); 
-    const target = req.params.setting
-    preferences.preferences.target = req.body.setting; 
+    preferences.preferences.birb = req.body.birb; 
 
-    // User.findByIdAndUpdate(req.params.user,
-    //     {$set: {
-    //         preferences: preferences.preferences
-    //     }},
-    //     {new: true}, 
-    //     function(err, user) {
-    //         if (err) {
-    //             res.status(400).json(err);
-    //         }else {
-    //             res.status(200).json(user);
-    //         }
-    //     }
-    // );
-    res.status(200).json(preferences.preferences); 
+    User.findByIdAndUpdate(req.params.user,
+        {$set: {
+            preferences: preferences.preferences
+        }},
+        {new: true}, 
+        function(err, user) {
+            if (err) {
+                res.status(400).json(err);
+            }else {
+                res.status(200).json(user);
+            }
+        }
+    );
+})
+
+/// PATCH Paper ---> DONE
+router.patch("/:user/paper", async (req, res) => { 
+    const preferences = await User.findById(req.params.user, "preferences"); 
+    preferences.preferences.paper = req.body.paper; 
+
+    User.findByIdAndUpdate(req.params.user,
+        {$set: {
+            preferences: preferences.preferences
+        }},
+        {new: true}, 
+        function(err, user) {
+            if (err) {
+                res.status(400).json(err);
+            }else {
+                res.status(200).json(user);
+            }
+        }
+    );
+})
+
+/// PATCH Font ---> DONE
+router.patch("/:user/font", async (req, res) => { 
+    const preferences = await User.findById(req.params.user, "preferences"); 
+    preferences.preferences.font = req.body.font; 
+
+    User.findByIdAndUpdate(req.params.user,
+        {$set: {
+            preferences: preferences.preferences
+        }},
+        {new: true}, 
+        function(err, user) {
+            if (err) {
+                res.status(400).json(err);
+            }else {
+                res.status(200).json(user);
+            }
+        }
+    );
+})
+
+/// PATCH wrkDur --->DONE
+router.patch("/:user/wrkDur", async (req, res) => { 
+    const preferences = await User.findById(req.params.user, "preferences"); 
+    preferences.preferences.wrkDur = req.body.wrkDur; 
+
+    User.findByIdAndUpdate(req.params.user,
+        {$set: {
+            preferences: preferences.preferences
+        }},
+        {new: true}, 
+        function(err, user) {
+            if (err) {
+                res.status(400).json(err);
+            }else {
+                res.status(200).json(user);
+            }
+        }
+    );
+})
+
+/// PATCH shrtBreak ---> DONE
+router.patch("/:user/shrtBreak", async (req, res) => { 
+    const preferences = await User.findById(req.params.user, "preferences"); 
+    preferences.preferences.shrtBreak = req.body.shrtBreak; 
+
+    User.findByIdAndUpdate(req.params.user,
+        {$set: {
+            preferences: preferences.preferences
+        }},
+        {new: true}, 
+        function(err, user) {
+            if (err) {
+                res.status(400).json(err);
+            }else {
+                res.status(200).json(user);
+            }
+        }
+    );
+})
+
+/// PATCH lngBreak
+router.patch("/:user/lngBreak", async (req, res) => { 
+    const preferences = await User.findById(req.params.user, "preferences"); 
+    preferences.preferences.lngBreak = req.body.lngBreak; 
+
+    User.findByIdAndUpdate(req.params.user,
+        {$set: {
+            preferences: preferences.preferences
+        }},
+        {new: true}, 
+        function(err, user) {
+            if (err) {
+                res.status(400).json(err);
+            }else {
+                res.status(200).json(user);
+            }
+        }
+    );
+})
+
+/// PATCH shrtPerLng
+router.patch("/:user/shrtPerLng", async (req, res) => { 
+    const preferences = await User.findById(req.params.user, "preferences"); 
+    preferences.preferences.shrtPerLng = req.body.shrtPerLng; 
+
+    User.findByIdAndUpdate(req.params.user,
+        {$set: {
+            preferences: preferences.preferences
+        }},
+        {new: true}, 
+        function(err, user) {
+            if (err) {
+                res.status(400).json(err);
+            }else {
+                res.status(200).json(user);
+            }
+        }
+    );
+})
+
+/// PATCH alert
+router.patch("/:user/alert", async (req, res) => { 
+    const preferences = await User.findById(req.params.user, "preferences"); 
+    preferences.preferences.alert = req.body.alert; 
+
+    User.findByIdAndUpdate(req.params.user,
+        {$set: {
+            preferences: preferences.preferences
+        }},
+        {new: true}, 
+        function(err, user) {
+            if (err) {
+                res.status(400).json(err);
+            }else {
+                res.status(200).json(user);
+            }
+        }
+    );
 })
 
 module.exports = router; 
