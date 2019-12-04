b9f3f1751f97e975e284b24248ef7d03a7c72d4b
aKwong-Taylor <a.kwong-taylor@outlook.com>
2019-12-04 15:48:44 -0800

    Fixes to several warnings displayed in browswer console.

--- client/src/components/Todo/TodoEdit.js
+++ client/src/components/Todo/TodoEdit.js
@@ -2,10 +2,6 @@
 import React, { Component } from 'react'
 
 export default class TodoEdit extends Component {
-    constructor(props) {
-        super(props); 
-    }
-
     render() {
         return (
             <div>
